6b524abd87f6d6148798c21ffda5c46534c34cdb
Pedro77h <Pedrohenriquearaujosoares25@gmail.com>
2022-11-18 21:14:36 -0300

    second commit

--- src/routes/mail.routes.ts
+++ src/routes/mail.routes.ts
@@ -4,7 +4,12 @@ import { Router } from "express";
 const mailRouter = Router()
 
 
-mailRouter.post('/send' , mailController.mail)
+mailRouter.post('/send', mailController.mail)
+mailRouter.get('/', (req, res) => {
+  res.send({
+    "hello":"world"
+  })
+})
 
 
 export default mailRouter
