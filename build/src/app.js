a9eb59ca95a48f2a4ca111b502a9c2b939773d49
Pedro77h <Pedrohenriquearaujosoares25@gmail.com>
2022-11-19 15:11:46 -0300

    Commit to hetoku

--- build/src/app.js
+++ build/src/app.js
@@ -23,7 +23,6 @@ class serverInit extends core_1.Server {
     setupExpress() {
         this.app.use(body_parser_1.default.json());
         this.app.use((0, cors_1.default)());
-        dotenv_1.default.config();
     }
     routes() {
         this.app.use("/mail", mail_routes_1.default);
