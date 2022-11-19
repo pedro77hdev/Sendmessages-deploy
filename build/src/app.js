2ad699cb2aaca756f478f41a194e21fd74ad151c
Pedro77h <Pedrohenriquearaujosoares25@gmail.com>
2022-11-19 15:07:32 -0300

    Commit to hetoku

--- build/src/app.js
+++ build/src/app.js
@@ -3,7 +3,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-require("module-alias/register");
 const dotenv_1 = __importDefault(require("dotenv"));
 const body_parser_1 = __importDefault(require("body-parser"));
 const cors_1 = __importDefault(require("cors"));
